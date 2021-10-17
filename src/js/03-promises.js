48723965fe122805c1878f7f30522aba42fa2ecd
Alexey Sapr <alexeysapr@gmail.com>
2021-10-17 20:26:40 +0300

    update task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,22 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+//генератор промисов
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    return Promise.resolve({ position, delay });
   } else {
     // Reject
+    return Promise.reject({ position, delay });
   }
 }
+
+//вызов генератора
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
